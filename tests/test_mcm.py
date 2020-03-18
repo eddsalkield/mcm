a879b9bd429c35b4c0d77c3e9cc5b69608493de9
Edd Salkield <edd@salkield.ukgit config --global user.email edd@salkield.ukgit config --global user.email edd@salkield.uk>
2020-03-18 21:17:10 +0000

    Repo cleanup

--- tests/test_mcm.py
+++ tests/test_mcm.py
@@ -1,5 +1,7 @@
 from mcm import __version__
-
+import pathlib
 
 def test_version():
-    assert __version__ == '0.1.0'
+    print('#############')
+    print(pathlib.Path.cwd())
+    assert __version__ == '0.0.2'
